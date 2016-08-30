74e4fd6a52e9d7f8410681f93674bb285acc435c
janson <gandancing@gmail.com>
2016-08-30 11:16:58 +0800

    reflect setting struct value

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/urfave/cli"
-	"github.com/urfave/cli/altsrc"
 )
 
 func main() {
@@ -13,16 +16,16 @@ func main() {
 	myApp.Name = "kcptun"
 	myApp.Usage = "kcptun client"
 	myApp.Flags = []cli.Flag{
-		altsrc.NewStringFlag(cli.StringFlag{
+		cli.StringFlag{
 			Name:  "localaddr,l",
 			Value: ":12948",
 			Usage: "local listen address",
-		}),
-		altsrc.NewStringFlag(cli.StringFlag{
+		},
+		cli.StringFlag{
 			Name:  "remoteaddr, r",
 			Value: "vps:29900",
 			Usage: "kcp server address",
-		}),
+		},
 		cli.StringFlag{
 			Name:  "c", // configuration file support
 			Usage: "config.json file support",
@@ -35,6 +38,39 @@ func main() {
 		return nil
 	}
 
-	myApp.Before = altsrc.InitInputSourceWithContext(myApp.Flags, NewJSONSourceFromFlagFunc("c"))
+	path := "config.json"
+	file, err := os.Open(path) // For read access.
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	var config map[string]interface{}
+	if err = json.NewDecoder(file).Decode(&config); err != nil {
+		log.Printf("error:%v\n", err)
+		return
+	}
+	for _, f := range myApp.Flags {
+		fValue := reflect.Indirect(reflect.ValueOf(f))
+		typeOfT := fValue.Type()
+		for i := 0; i < fValue.NumField(); i++ {
+			ff := fValue.Field(i)
+			fmt.Printf("%d: %s %s = %v %v\n", i,
+				typeOfT.Field(i).Name, ff.Type(), ff.Interface(), ff.CanSet())
+		}
+
+		varValue := fValue.FieldByName("Name")
+		if !varValue.IsValid() {
+			return
+		}
+		varStr := varValue.String()
+		vars := strings.Split(varStr, ",")
+		varStr = strings.TrimSpace(vars[0])
+		if varJson, err := jsonGetValue(varStr, config); err == nil {
+			log.Printf("varValue=%s varJson=%v\n", varStr, varJson)
+		}
+	}
+
+	//myApp.Before = altsrc.InitInputSourceWithContext(myApp.Flags, NewJSONSourceFromFlagFunc("c"))
 	myApp.Run(os.Args)
 }
